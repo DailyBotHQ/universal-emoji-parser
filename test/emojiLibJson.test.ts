2681ed7eefbfd303768dfeff5c332000188e59f8
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2023-05-01 21:28:00 -0500

    Adding support to export emoji lib json data

--- test/emojiLibJson.test.ts
+++ test/emojiLibJson.test.ts
@@ -13,46 +13,42 @@ describe('Test emoji lib json data', () => {
       // (2) Check emojiLibJsonData values
       let emojiObjectValue: EmojiType = emojiLibJsonData['🤣']
       expect(emojiObjectValue).to.be.an('object')
-      expect(emojiObjectValue).to.be.deep.equal(
-        {
-          name: 'rolling on the floor laughing',
-          slug: 'rolling_on_the_floor_laughing',
-          group: 'Smileys & Emotion',
-          emoji_version: '3.0',
-          unicode_version: '3.0',
-          skin_tone_support: false,
-          char: '🤣',
-          keywords: [
-            'rolling_on_the_floor_laughing',
-            'rolling',
-            'floor',
-            'laughing',
-            'rofl'
-          ]
-        }
-      )
+      expect(emojiObjectValue).to.be.deep.equal({
+        name: 'rolling on the floor laughing',
+        slug: 'rolling_on_the_floor_laughing',
+        group: 'Smileys & Emotion',
+        emoji_version: '3.0',
+        unicode_version: '3.0',
+        skin_tone_support: false,
+        char: '🤣',
+        keywords: [
+          'rolling_on_the_floor_laughing',
+          'rolling',
+          'floor',
+          'laughing',
+          'rofl',
+        ],
+      })
 
       // (3) Check emojiLibJsonData values
       emojiObjectValue = emojiLibJsonData['😎']
       expect(emojiObjectValue).to.be.an('object')
-      expect(emojiObjectValue).to.be.deep.equal(
-        {
-          name: 'smiling face with sunglasses',
-          slug: 'smiling_face_with_sunglasses',
-          group: 'Smileys & Emotion',
-          emoji_version: '1.0',
-          unicode_version: '1.0',
-          skin_tone_support: false,
-          char: '😎',
-          keywords: [
-            'smiling_face_with_sunglasses',
-            'cool',
-            'summer',
-            'beach',
-            'sunglass'
-          ]
-        }
-      )
+      expect(emojiObjectValue).to.be.deep.equal({
+        name: 'smiling face with sunglasses',
+        slug: 'smiling_face_with_sunglasses',
+        group: 'Smileys & Emotion',
+        emoji_version: '1.0',
+        unicode_version: '1.0',
+        skin_tone_support: false,
+        char: '😎',
+        keywords: [
+          'smiling_face_with_sunglasses',
+          'cool',
+          'summer',
+          'beach',
+          'sunglass',
+        ],
+      })
     })
   })
 })
