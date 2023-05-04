c1187676cc9aaa442c2d7298e4b46d17f1722cb7
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2023-05-04 10:51:51 -0500

    Updating github releases script

--- get_github_release_log.sh
+++ get_github_release_log.sh
@@ -8,7 +8,7 @@ while read text_line; do
   if [[ "$text_line" =~ "[🤖 DailyBot] New release to v" ]]; then
     break
   fi
-  if [[ ! "$text_line" =~ "Merge branch 'main'" ]] || [[ ! "$text_line" =~ "Merge pull request " ]]; then
+  if [[ ! "$text_line" =~ "Merge branch 'main'" ]] && [[ ! "$text_line" =~ "Merge pull request" ]]; then
     echo "🚩 $text_line" >> git_logs_output.txt
   fi
 done < git_logs.txt
